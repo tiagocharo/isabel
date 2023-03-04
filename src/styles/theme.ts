0965c00042f82f48830c2573ce413ccac60ec6a2
Tiago Rocha <rocha@winsocial.com.br>
2023-03-04 20:25:39 -0300

    new logo

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,9 @@
 const theme = {
-  colorPrimary: '#E88D5D',
-  colorSecondary: '#AC897E',
-  colorTerciary: '#FFF3E2',
-  colorLightSecondary: '#FFCBAB',
-  headerHeight: '70px',
-}
+  colorPrimary: "#fda5ac",
+  colorSecondary: "#AC897E",
+  colorTerciary: "#FFF3E2",
+  colorLightSecondary: "#FFCBAB",
+  headerHeight: "70px",
+};
 
-export default theme
+export default theme;
